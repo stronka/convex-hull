f223527bbbdf7dbf50edbaf52a5ceb84cd36834d
unknown <stronka.k@gmail.com>
2019-10-14 09:53:49 +0200

    change test gift wrapping tests to create point vectors explicitly

--- test/algorithm/test_gift_wrapping.cpp
+++ test/algorithm/test_gift_wrapping.cpp
@@ -22,10 +22,10 @@ TEST test_BuildHull_OneElement_HullContainsOneElement(){
 	MockPointCollectionBuilder mockBuilder;
 
 	Point2D p(1., 2.);
-	PointVectorBuilder b;
-	b.addPoint(p);
 
-	GiftWrappingAlgorithm::buildHull(b.build(), mockBuilder);
+	std::vector <Point2D> points = {p};
+
+	GiftWrappingAlgorithm::buildHull(points, mockBuilder);
 
 	ASSERT(mockBuilder.points.size() == 1, "Fail.");
 }
@@ -35,11 +35,12 @@ TEST test_BuildHull_TwoElements_HullContainsTwoElements(){
 
 	Point2D p1(1., 2.);
 	Point2D p2(2., 2.);
-	PointVectorBuilder b;
-	b.addPoint(p1);
-	b.addPoint(p2);
 
-	GiftWrappingAlgorithm::buildHull(b.build(), mockBuilder);
+	std::vector <Point2D> points = {
+		p1, p2
+	};
+
+	GiftWrappingAlgorithm::buildHull(points, mockBuilder);
 
 	ASSERT(mockBuilder.points.size() == 2, "Fail.");
 }
@@ -51,12 +52,11 @@ TEST test_BuildHull_ThreeElements_HullContainsThreeElements(){
 	Point2D p2(2., 2.);
 	Point2D p3(2., 3.);
 
-	PointVectorBuilder b;
-	b.addPoint(p1);
-	b.addPoint(p2);
-	b.addPoint(p3);
+	std::vector <Point2D> points = {
+		p1, p2, p3
+	};
 
-	GiftWrappingAlgorithm::buildHull(b.build(), mockBuilder);
+	GiftWrappingAlgorithm::buildHull(points, mockBuilder);
 
 	ASSERT(mockBuilder.points.size() == 3, "Fail.");
 }
@@ -69,15 +69,11 @@ TEST test_BuildHull_AllElementsOnAHull_HullContainsAllElements(){
 	Point2D p3(2., 1.);
 	Point2D p4(1., 2.);
 	Point2D p5(0., 2.);
-	PointVectorBuilder b;
-	b.addPoint(p1);
-	b.addPoint(p2);
-	b.addPoint(p3);
-	b.addPoint(p4);
-	b.addPoint(p5);
 
+	std::vector <Point2D> points = {
+		p1, p2, p3, p4, p5
+	};
 
-	std::vector <Point2D> points = b.build();
 	GiftWrappingAlgorithm::buildHull(points, mockBuilder);
 
 	ASSERT(mockBuilder.points.size() == 5, "Fail. Got %d, want %d", mockBuilder.points.size(), points.size());
@@ -92,14 +88,10 @@ TEST test_BuildHull_OneElementInsideull_HullSizeIsLessThanPointsSizeByOne(){
 	Point2D p4(1., 1.);
 	Point2D p5(0., 2.);
 
-	PointVectorBuilder b;
-	b.addPoint(p1);
-	b.addPoint(p2);
-	b.addPoint(p3);
-	b.addPoint(p4);
-	b.addPoint(p5);
+	std::vector <Point2D> points = {
+		p1, p2, p3, p4, p5
+	};
 
-	std::vector <Point2D> points = b.build();
 	GiftWrappingAlgorithm::buildHull(points, mockBuilder);
 
 	ASSERT(mockBuilder.points.size() == points.size() - 1, "Fail. Got %d, want %d", mockBuilder.points.size(), points.size() - 1);
@@ -121,19 +113,10 @@ TEST test_BuildHull_ManyElementsInsideull_HullSizeIsCorrect(){
 	Point2D p9(1.2, 1.);
 	Point2D p10(1.2, 1.2);
 
-	PointVectorBuilder b;
-	b.addPoint(p1);
-	b.addPoint(p2);
-	b.addPoint(p3);
-	b.addPoint(p4);
-	b.addPoint(p5);
-	b.addPoint(p6);
-	b.addPoint(p7);
-	b.addPoint(p8);
-	b.addPoint(p9);
-	b.addPoint(p10);
+	std::vector <Point2D> points = {
+		p1, p2, p3, p4, p5, p6, p7, p8, p9, p10
+	};
 
-	std::vector <Point2D> points = b.build();
 	GiftWrappingAlgorithm::buildHull(points, mockBuilder);
 
 	ASSERT(mockBuilder.points.size() == 4, "Fail. Got %d, want %d", mockBuilder.points.size(), 4);
@@ -150,14 +133,10 @@ TEST test_BuildHull_FirstElementIsLeftmost_HullSizeIsCorrect(){
 	//remaining points
 	Point2D p5(1., 1.);
 
-	PointVectorBuilder b;
-	b.addPoint(p1);
-	b.addPoint(p2);
-	b.addPoint(p3);
-	b.addPoint(p4);
-	b.addPoint(p5);
+	std::vector <Point2D> points = {
+		p1, p2, p3, p4, p5
+	};
 
-	std::vector <Point2D> points = b.build();
 	GiftWrappingAlgorithm::buildHull(points, mockBuilder);
 
 	ASSERT(mockBuilder.points.size() == 4, "Fail. Got %d, want %d", mockBuilder.points.size(), 4);
@@ -174,23 +153,13 @@ TEST test_FindHull_FirstElementIsLeftmost_HullIsCorrect(){
 	//remaining points
 	Point2D p5(1., 1.);
 
-	PointVectorBuilder b;
+	std::vector <Point2D> points = {
+		p1, p2, p3, p4, p5
+	};
 
-	b.addPoint(p1);
-	b.addPoint(p2);
-	b.addPoint(p3);
-	b.addPoint(p4);
-	b.addPoint(p5);
-
-	std::vector <Point2D> points = b.build();
-
-	b.reset();
-	b.addPoint(p1);
-	b.addPoint(p4);
-	b.addPoint(p3);
-	b.addPoint(p2);
-
-	std::vector <Point2D> expected = b.build();
+	std::vector <Point2D> expected = {
+		p1, p4, p3, p2
+	};
 
 	GiftWrappingAlgorithm::buildHull(points, mockBuilder);
 	ASSERT(mockBuilder.points == expected, "Fail.");
